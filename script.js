b4fe3a4294b2afb5f44370fd85de954e413c5875
Moulik-6 <193782932+Moulik-6@users.noreply.github.com>
2025-04-07 20:52:07 +0530

    create 2d array to store div efficiently

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector("div");
+const div = [[],[]];
 
+for(let i = 1; i < 2; i++){
+    for(let j = 1; j < 2; j++){
+        div[[i],[j]] = document.createElement("div");
 
-for(let i = 0; i < 16; i++){
-    const div = document.createElement("div");
+        container.appendChild(div[[i],[j]]);
 
-    container.appendChild(div);
+    }
 }
