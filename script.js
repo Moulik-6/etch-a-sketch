7e225946c06e5c5840da66c0be8fc87ecdbd1824
Moulik-6 <193782932+Moulik-6@users.noreply.github.com>
2025-04-07 19:53:28 +0530

    deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector(".container");
-
-for(let i = 0; i < 17; i++){
-    const div = [];
-
-    div[i] = document.createElement("div");
-
-    container.appendChild(div[i]);
-}
